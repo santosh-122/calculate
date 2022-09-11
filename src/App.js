12e15ba64a8482a8d0ad4f701377df33d1419f9b
santosh-122 <santoshkumark122@gmail.com>
2022-09-11 20:04:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+
+
+// import './App.css';
+// import React,{useState} from 'react'
+
+// function App() {
+//   const [count,setCount] = useState(false)
+//   const [value,setvalue]  = useState([])
+// //   function stop(){
+// //   for(let i=1;i<=1;i++){
+// //     console.log("stop")
+// //   }
+ 
+// // }
+// function coun(){
+// for(let i=1;i>=0;i++)
+// {
+//    setvalue(i);
+//    return value
+// }
+// }
+//   return (
+//     <div className="App">
+     
+//       <button onClick={() => setCount(true)}>start</button>
+//        <button onClick={() => setCount(false)}>stop</button>
+       
+//       {count && 
+//                 {coun}
+           
+//       } 
+//       {coun}
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// import './App.css';
+// import StopWatch from './Components/StopWatch';
+
+// function App() {
+// return (
+//   <div className="App">
+//   <StopWatch />
+//   </div>
+// );
+// }
+
+// export default App;
+
+
+function x(){
+  var y=10,z=20;
+  return function b(){
+  console.log(y)
+  }
+}
+var t = x()
+t();
